133e83c2f130aafa3e97b45d9c8289eb29b74773
reddysaragada9052 <reddysaragada9052@gmail.com>
2025-08-30 18:18:10 +0100

    Model training is completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
-
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> Stage : {STAGE_NAME} started <<<<<<") 
@@ -34,3 +34,16 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Model Training stage"
+try:
+   logger.info(f">>>>>> Stage : {STAGE_NAME} started <<<<<<") 
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.initiate_model_training()
+   logger.info(f">>>>>> Stage : {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -1,6 +1,6 @@
 from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
-from src.datascience.entity.config_entity import (DataIngestionConfig, DataValidationConfig, Data_TransformationConfig)
+from src.datascience.entity.config_entity import (DataIngestionConfig, DataValidationConfig, Data_TransformationConfig, ModelTrainerConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -51,3 +51,22 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir = config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+        )
+
+        return model_trainer_config
